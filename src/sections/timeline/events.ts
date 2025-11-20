5ef901fee761af3c8d7f184e95399dd4b3191d85
Juan Sebastian Gomez <js.gomezt21@gmail.com>
2025-11-20 18:29:09 -0500

    photos added

--- src/sections/timeline/events.ts
+++ src/sections/timeline/events.ts
@@ -18,91 +18,256 @@ const events: TimelineEventType[] = [
     title: "El día que decidimos ser novios",
     description:
       "Después de 2 meses de salir casi todos los días, tener muchas conversaciones, fiestas, y momentos juntos, decidimos dar el siguiente paso en nuestra relación y comprometernos a ser novios oficialmente.",
-    images: [],
+    images: [
+      {
+        path: "timeline/6E819DD5-DC2B-4BC2-BB0B-B6E963C0F9CD.jpeg",
+        alt: "Stella",
+      },
+      {
+        path: "timeline/IMG_6273.png",
+        alt: "Stella",
+      },
+    ],
   },
   {
     date: "7 de junio de 2022",
     title: "Nuestro primer viaje juntos",
     description:
       "Nuestro primer viaje juntos, después de una semana donde intenté correr una media maratón, decidimos que al volver íbamos a ir a Guatavita para despejarnos y estar juntos pues era la primera vez que nos separábamos por tantos días sin vernos.",
-    images: [],
+    images: [
+      {
+        path: "timeline/IMG_6550.png",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/3606675C-CFA5-4403-A202-1B6FD6819F3D.JPG",
+        alt: "Stella",
+      },
+    ],
   },
   {
     date: "junio de 2022",
     title: "Nuestro primer viaje en familia",
     description:
       "Hicimos un roadtrip con tu mami y sus amigos a Bucaramanga, hicimos una parada en Barichara donde no les gustaron los raviolis de salmón y luego llegamos a Bucaramanga donde conocí a la abi, a tu tío, Rafaella y Andreita.",
-    images: [],
+    images: [
+      {
+        path: "timeline/IMG_0405.png",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/299641010_1097952517488574_5226957228036085843_n.jpg",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/PHOTO-2022-07-06-11-11-26.jpg",
+        alt: "Guatavita",
+      },
+    ],
   },
   {
     date: " 3 de diciembre de 2022",
     title: "Primer viaje internacional juntos",
     description:
       "Fuimos a Ciudad de México, conocimos muchos lugares y tuvimos nuestra primera experiencia compartiendo los gustos del otro en los viajes, como ir a museos o caminatas por el parque, hasta ir a parques de diversiones y romper gafas en las atracciones.",
-    images: [],
+    images: [
+      {
+        path: "timeline/IMG_3509.JPG",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/IMG_1264.png",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/PHOTO-2023-10-17-23-17-26.jpg",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/IMG_3781.png",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/IMG_1752.png",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/IMG_1717.png",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/FullSizeRender 2.png",
+        alt: "Guatavita",
+      },
+    ],
   },
   {
     date: "Marzo de 2023",
     title: "Roadtrip Boyacá en pareja",
     description:
       "Fuimos a un roadtrip por Boyacá donde visitamos varios pueblos como Villa de Leyva, Raquira, y otros! Nos quedamos en una casita en Iza súper bonita y la pasamos increíble.",
-    images: [],
+    images: [
+      {
+        path: "timeline/IMG_2171.png",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/IMG_5702.JPG",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/IMG_2156.png",
+        alt: "Guatavita",
+      },
+    ],
   },
   {
     date: "Mayo de 2023",
     title: "Celebraciones de aniversario!",
     description:
       "Para este año decidimos tener 2 celebraciones de aniversario una escapada a un Glamping y una noche de hotel en la ciudad donde vimos películas y cenamos muchísimo! La decisión se basó principalmente en que yo había viajado con mi familia y no habíamos podido compartir mucho! ",
-    images: [],
+    images: [
+      {
+        path: "timeline/IMG_2557.png",
+        alt: "Guatavita",
+      },
+      {
+        path: "timeline/IMG_2577.png",
+        alt: "Guatavita",
+      },
+    ],
   },
   {
     date: "Agosto de 2023",
     title: "Vieja por la costa oeste USA",
     description:
       "Viajamos a la costa oeste de USA, conocimos Los Angeles, San Francisco y Las Vegas! Fuimos a Disney y Universal, conocimos Standford, fuimos a Google, Facebook, y Apple, y la pasamos increíble! Un viaje realmente inolvidable.",
-    images: [],
+    images: [
+      {
+        path: "timeline/c2463277-b574-4d0e-bcaa-7a194d0c27bd.JPG",
+        alt: "spiderman",
+      },
+      {
+        path: "timeline/7a012885-0eba-460c-86d4-ff4e7add873b.JPG",
+        alt: "serios",
+      },
+      {
+        path: "timeline/391219414_1272647530101867_3960500254608168556_n.jpg",
+        alt: "serios",
+      },
+      {
+        path: "timeline/391404985_1063879457953125_8496308470997630011_n.jpg",
+        alt: "serios",
+      },
+      {
+        path: "timeline/390925544_707608744120658_9220528113338430346_n.jpg",
+        alt: "serios",
+      },
+    ],
   },
   {
     date: "Septiembre de 2023",
     title: "Graduación de la universidad",
     description:
       "Fue un día en el que te graduaste! Después de todo tu esfuerzo y de todo el perrenque que tuviste que meterle, lo lograste. Me sentí muy orgulloso y feliz de verte triunfar y acompañarte en ese momento tan especial.",
-    images: [],
+    images: [
+      {
+        path: "timeline/PHOTO-2023-09-26-21-07-33.jpg",
+        alt: "serios",
+      },
+      {
+        path: "timeline/PHOTO-2023-09-08-23-20-49.jpg",
+        alt: "serios",
+      },
+    ],
   },
   {
     date: "Diciembre 2023",
     title: "Un Diciembre de primeras veces",
     description:
       "Pasamos nuestra primera noche de velitas juntos, primera navidad y fin de año juntos, la navidad con tu familia en Santander y fin de año en Bogotá.",
-    images: [],
+    images: [
+      {
+        path: "timeline/IMG_3660.JPG",
+        alt: "serios",
+      },
+      {
+        path: "timeline/IMG_3603.png",
+        alt: "serios",
+      },
+    ],
   },
   {
     date: "Abril/Mayo 2024",
     title: "A vivir en Europa por 2 meses",
     description:
       "Terminando tu maestría tenías que hacer prácticas presenciales en Europa, así que decidimos arrancar a Madrid y vivir cerca de la gran vía. Aprovechamos nuestro tiempo para conocer Toledo, Barcelona, Londres, Liverpool, Manchester y hasta perdimos un bus hacia Portugal. Nos reimos, y una vez más seguimos confirmando que somos el uno para el otro.",
-    images: [],
+    images: [
+      {
+        path: "timeline/447956862_951455296751824_8885447330930019067_n.jpg",
+        alt: "serios",
+      },
+      {
+        path: "timeline/441280507_3726871564306053_2215302926940378124_n.jpg",
+        alt: "serios",
+      },
+      {
+        path: "timeline/PHOTO-2024-05-27-15-51-29.jpg",
+        alt: "serios",
+      },
+      {
+        path: "timeline/PHOTO-2024-05-29-11-31-04.jpg",
+        alt: "serios",
+      },
+    ],
   },
   {
     date: "13 de julio 2024",
     title: "Comprando a casita!!!!",
     description:
       "Después de ir a mirar apartamentos para arrendar o comprar, decidimos invertir en nuestro hogar juntos. Un proyecto llamado Nahiku, el cual vamos a visitar hoy en día y ya está casi terminado, hemos trabajado mucho y hemos dado todo de nosotros para que esta casita se construya con mucho amor y ya a la espera de que nos la entreguen!",
-    images: [],
+    images: [
+      {
+        path: "timeline/WhatsApp Image 2024-07-13 at 12.50.24.jpeg",
+        alt: "comprando casita",
+      },
+    ],
   },
   {
     date: "Febrero de 2025",
     title: "Matrimonio familiar",
     description:
       "Mi hermano se casa, nos acompañas y eres una persona que me acompaño en todo! Me ayudaste a escoger el smoking correcto, ayudaste a mi hermano a que comprara su traje, a mi mamá con el vestido y nos apoyaste en todo el proceso, logrando que todo fuera mágico e inolvidable.",
-    images: [],
+    images: [
+      {
+        path: "timeline/1d0a212d-9f76-40d4-a36a-bcb764d9fbc2.JPG",
+        alt: "matrimonio hermano",
+      },
+    ],
   },
   {
     date: "Septiembre de 2025",
     title: "Disney, Universal, Epic!",
     description:
       "Después de nuestra boda, nos vamos de viaje de bodas a un lugar paradisíaco donde podamos relajarnos y disfrutar de nuestra nueva vida juntos como esposos.",
-    images: [],
-  }
+    images: [
+      {
+        path: "timeline/560555736_18541666585019285_8117239761615542323_n.jpg",
+        alt: "matrimonio hermano",
+      },
+      {
+        path: "timeline/560651585_18541666570019285_2612193162202382234_n.jpg",
+        alt: "matrimonio hermano",
+      },
+      {
+        path: "timeline/561643119_18541666501019285_6040109555971369206_n.jpg",
+        alt: "matrimonio hermano",
+      },
+      {
+        path: "timeline/B5C034C6-952F-47DB-9C45-0CDF57B8C9CE.JPG",
+        alt: "matrimonio hermano",
+      },
+    ],
+  },
 ];
 export default events;
